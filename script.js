bf7cac8eec387dc9670918bde8786b5804df248a
Malaya Maharathi <121949566+Alex-963@users.noreply.github.com>
2023-05-30 11:44:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let images = ["⚽", "😁", "🎇", "♥", "🚩", "✨", "🧿", "😂", "🌺", "😍", "😋", "🖖", "🚀", "🎃", "🎄", "🎀", "🎵", "🍓", "😋"];
+document.addEventListener('mousemove', (e) => {
+    let body = document.querySelector('body');
+    let emoji = document.createElement('span');
+    let x = e.offsetX;
+    let y = e.offsetY;
+
+    emoji.style.left = x + 'px';
+    emoji.style.top = y + 'px';
+
+    let icon = images[Math.floor(Math.random() * images.length)];
+    emoji.innerHTML = icon;
+
+    let size = Math.random() * 50;
+    emoji.style.fontSize = 5 + size + 'px';
+
+    let max = 0;
+    let min = 200;
+    let randomValue = Math.floor((Math.random() * ((max + 1) - min)) + min);
+
+    emoji.style.transform = `translateX(${randomValue}px)`;
+
+    body.appendChild(emoji);
+
+    setTimeout(() => {
+        emoji.remove();
+    }, 1000);
+});
